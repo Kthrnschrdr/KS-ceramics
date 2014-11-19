54e7f19095e87743a20733ff770e8ca77e39ba0b
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-11-19 11:48:46 -0600

    still shuffling and fixed loop for slideshow

--- themes/my_theme/page.php
+++ themes/my_theme/page.php
@@ -5,9 +5,9 @@ if (have_posts()) :
    while (have_posts()) :
       the_post();
 ?>
-<?php echo do_shortcode('[portfolio_slideshow]');?>
 <div class="content">
   <?php the_content(); ?>
+  <?php echo do_shortcode('[portfolio_slideshow]');?>
 </div>
 <?php
    endwhile;
