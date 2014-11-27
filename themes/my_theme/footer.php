c3352a0f9202468211d572a6f92e5c24603aedfe
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-11-26 21:40:32 -0600

    still fixing tiny things' '

--- themes/my_theme/footer.php
+++ themes/my_theme/footer.php
@@ -1,7 +1,7 @@
 	<?php wp_footer(); ?>
   <div class="footer">
     <ul>
-      <li><a href="contact/">contact</a></li>
+      <li><a href="contact/">email</a></li>
       <li><img src="http://i.imgur.com/sXCGNQc.jpg"></img></li>
       <li><a href="https://www.facebook.com/pages/Kathryn-Schroeder-Ceramics/1513074302238647">facebook</a></li>
     </ul>
